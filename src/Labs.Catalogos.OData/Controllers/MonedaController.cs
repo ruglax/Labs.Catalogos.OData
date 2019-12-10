0a83afee8ff789e2a0b31fe0858cc739e2ffcb4d
Raúl Silva <raul.silva@contpaqi.com>
2019-12-10 17:12:56 -0600

    Added Moneda catalog

---
+++ src/Labs.Catalogos.OData/Controllers/MonedaController.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Labs.Catalogos.OData.Internal;
+using Labs.Excel.Loader.Model;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Cors;
+
+namespace Labs.Catalogos.OData.Controllers
+{
+    [EnableQuery]
+    [EnableCors(Constantes.CorsPolicy)]
+    public class MonedaController : ODataBaseController
+    {
+        public IQueryable<c_Moneda> Get()
+        {
+            return DbCatalogContext.Moneda;
+        }
+    }
+}
